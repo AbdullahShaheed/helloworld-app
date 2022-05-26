3d193fa82e45476b513249b0a4c428d901db5d1d
AbdullahShaheed <73660907+AbdullahShaheed@users.noreply.github.com>
2022-05-26 23:42:14 +0300

    Update index.js
    Third dot added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello World..</h1>; //jsx expression
+const element = <h1>Hello World...</h1>; //jsx expression
 console.log(element);
 ReactDOM.render(element, document.getElementById("root"));
